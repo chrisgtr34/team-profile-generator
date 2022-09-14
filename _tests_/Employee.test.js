a5737421a7628966b99c0cfb154c6bfcd1650c05
CHRIS <chris@CHRISs-MBP.attlocal.net>
2022-09-13 22:32:34 -0700

    creating test folder starting with employee

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,8 @@
+const Employee = require('../lib/Employee');
+
+describe('Test the Employee Class',() => {
+    IDBTransaction('should create a new employee', () => {
+        const me = new Employee;
+        
+    }
+}
