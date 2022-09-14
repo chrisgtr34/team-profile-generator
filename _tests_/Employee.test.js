40ed54c34840ecba6f059b1af831938f2ad41c98
CHRIS <chris@CHRISs-MBP.attlocal.net>
2022-09-13 22:48:09 -0700

    adding onto test folder for employee

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,8 +1,12 @@
 const Employee = require('../lib/Employee');
 
 describe('Test the Employee Class',() => {
-    IDBTransaction('should create a new employee', () => {
-        const me = new Employee;
-        
-    }
-}
+    it('should create a new employee', () => {
+        const me = new Employee (1, 'chrisgtr34@gmail.com', 'chris');
+        expect(typeof me).toBe('object');
+    })
+    it('set a name via constructor', () => {
+        const me = new Employee (1, 'chrisgtr34@gmail.com', 'chris');
+        expect(me.name).toEqual('chris');
+    })
+})
