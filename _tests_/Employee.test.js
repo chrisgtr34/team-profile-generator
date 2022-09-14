8ccc756485d07c16fb10c1e249eb42ced07c3e2f
CHRIS <chris@CHRISs-MBP.attlocal.net>
2022-09-13 23:02:46 -0700

    updating employee test function

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -9,4 +9,29 @@ describe('Test the Employee Class',() => {
         const me = new Employee (1, 'chrisgtr34@gmail.com', 'chris');
         expect(me.name).toEqual('chris');
     })
+    it('set a id via constructor', () => {
+        const me = new Employee (1, 'chrisgtr34@gmail.com', 'chris');
+        expect(me.id).toEqual(1);
+    })
+    it('set a email via constructor', () => {
+        const me = new Employee (1, 'chrisgtr34@gmail.com', 'chris');
+        expect(me.email).toEqual('chrisgtr34@gmail.com');
+    })
+    it('can obtain email via getEmail() method', () => {
+        const me = new Employee (1, 'chrisgtr34@gmail.com', 'chris');
+        expect(me.getEmail()).toEqual('chrisgtr34@gmail.com');
+    })
+    it('can obtain name via getName() method', () => {
+        const me = new Employee (1, 'chrisgtr34@gmail.com', 'chris');
+        expect(me.get.Name).toEqual('chris');
+    })
+    it('can obtain id via getID() method', () => {
+        const me = new Employee (1, 'chrisgtr34@gmail.com', 'chris');
+        expect(me.getId()).toEqual(1);
+    })
+    it('can obtain role via getRole() method', () => {
+        const me = new Employee (1, 'chrisgtr34@gmail.com', 'chris');
+        expect(me.getRole()).toEqual('Employee');
+    })
+    
 })
