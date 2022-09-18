e8439bcf749606bdcbc7a267fcff6d4b0710511c
CHRIS <chris@CHRISs-MBP.attlocal.net>
2022-09-18 16:01:23 -0700

    adding icons to employee to distinguish their positions in a symbolic form

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -6,11 +6,11 @@ const generateTeam = function (team) {
         <article class="col-md-4 ">
             <div class="card">
                 <div class="card-header" style="background-color: red; color:white;">
-                    <h2>${manager.getName()}</h2>
+                    <h2>${manager.getName()}<i class="fa-solid fa-mug-hot"></i></h2>
                 </div>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">ID:${manager.getId()}</li>
-                    <li class="list-group-item">Email: ${manager.getEmail()}</li>
+                    <li class="list-group-item">Email: <a href = "mailto:${manager.getEmail()}">${manager.getEmail()}<a/></li>
                     <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
                 </ul>
             </div>
@@ -24,33 +24,33 @@ const generateTeam = function (team) {
         <article class="col-md-4 ">
             <div class="card">
                 <div class="card-header" style="background-color: red; color:white;">
-                    <h2>${engineer.getName()}</h2>
+                    <h2>${engineer.getName()}<i class="fa-solid fa-laptop-code"></i></h2>
                 </div>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">ID:${engineer.getId()}</li>
-                    <li class="list-group-item">Email: ${engineer.getEmail()}</li>
-                    <li class="list-group-item">GitHub: <a href="https://www.github.com/${engineer.getGitHub()}>${engineer.getGitHub()}L</a></li>
+                    <li class="list-group-item">Email: <a href = "mailto:${engineer.getEmail()}">${engineer.getEmail()}<a/></li>
+                    <li class="list-group-item">GitHub: <a href="https://www.github.com/${engineer.getGitHub()}">${engineer.getGitHub()}L</a></li>
                 </ul>
             </div>
         </article>  
         `; 
-    })
+    });
     team.interns.forEach(function(intern) {
         html += `
         <article class="col-md-4 ">
             <div class="card">
                 <div class="card-header" style="background-color: red; color:white;">
-                    <h2>${intern.getName()}</h2>
+                    <h2>${intern.getName()}<i class="fa-solid fa-user-graduate"></i></h2>
                 </div>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">ID:${intern.getId()}</li>
-                    <li class="list-group-item">Email: ${intern.getEmail()}</li>
-                    <li class="list-group-item">School: <a href = "mailto:${intern.getSchool()}">${intern.getSchool()}</a></li>
+                    <li class="list-group-item">Email: <a href = "mailto: ${intern.getEmail()}">${intern.getEmail()}</a></li>
+                    <li class="list-group-item">School: ${intern.getSchool()}</li>
                 </ul>
             </div>
         </article>  
         `; 
-    })
+    });
 
  return html;
 }
@@ -68,7 +68,8 @@ const generateHTML = function (team) {
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
             integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" 
+            integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <title>Team Profile Generator</title>
     </head>
     
